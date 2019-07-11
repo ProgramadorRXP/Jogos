62dea98d5ddfda8fd45b140a6ee15313d1e82c5d
ProgramadorRXP <51250848+ProgramadorRXP@users.noreply.github.com>
2019-07-11 13:39:22 -0300

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,25 @@
+﻿{
+	"version": 1562861503,
+	"fileList": [
+		"data.js",
+		"jogo1.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/p_real-sheet0.png",
+		"images/personagem-sheet0.png",
+		"images/personagem-sheet1.png",
+		"images/sprite-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"images/sprite3-sheet0.png",
+		"images/sprite4-sheet0.png",
+		"images/sprite5-sheet0.png",
+		"images/sprite7-sheet0.png",
+		"images/jogar-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
